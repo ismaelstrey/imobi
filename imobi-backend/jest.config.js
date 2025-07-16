fa8eba875d4598ab4dfa6efa64d4b96ad6daa164
ismael strey pereira <ismaelstrey@hotmail.com>
2025-07-16 12:38:04 -0300

    refactor(jest): update jest config to use transform options
    Move ts-jest configuration from globals to transform options for better compatibility with newer jest versions

--- imobi-backend/jest.config.js
+++ imobi-backend/jest.config.js
@@ -1,15 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json'
-    }
-  },
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\.ts$': ['ts-jest', {
+      tsconfig: 'tsconfig.test.json'
+    }],
   },
   collectCoverageFrom: [
     'src/**/*.ts',
